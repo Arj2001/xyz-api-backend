b744b4b54b83e6ddd8d0dc7b1f770cc19b4c7820
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-18 22:32:21 +0530

    Initial commit

---
+++ src/main/java/com/nestdigital/xyzapp/XyzAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzAppApplication.class, args);
+	}
+
+}
